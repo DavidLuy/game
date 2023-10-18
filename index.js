909e0da9bcac21873c8d6f7eeaf77a9769821c5e
DavidLuy <>
2023-10-18 16:11:56 +0000

    setting game timer on page

---
+++ index.js
@@ -0,0 +1,19 @@
+const ROCK = 'ROCK';
+const SCISSORS = 'SCISSORS';
+const PAPER = 'PAPER';
+const SPOCK = 'SPOCK';
+const LIZARD = 'LIZARD';
+const OPTIONS = {
+    1: ROCK,
+    2: SCISSORS,
+    3: PAPER
+    4: SPOCK,
+    5: LIZARD
+};
+const OPTIONS2EMOJI = {
+    1: '⛰️',
+    2: '✂️',
+    3: '🧻',
+    4: '🖖',
+    5: '🦎'
+};
